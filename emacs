489e61bdf4092f8249831950d8012317194e3b27
Tom Fruchterman <tfruchterman@hotmail.com>
2012-11-08 10:48:11 -0800

    emacs aliases emacs.d ipython

---
+++ emacs
@@ -0,0 +1,173 @@
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+(setq org-agenda-files (list "~/notes/notes.org"))
+
+(setq org-directory "~/notes/")
+(setq org-default-notes-file "~/notes/remember.org")
+(setq remember-annotation-functions '(org-remember-annotation))
+(setq remember-handler-functions '(org-remember-handler))
+(add-hook 'remember-mode-hook 'org-remember-apply-template)
+(define-key global-map "\C-cr" 'org-remember)
+
+
+(defun shell-2 () (interactive) (shell "*shell*<2>"))
+
+(defun shell-3 () (interactive) (shell "*shell*<3>"))
+(defun shell-4 () (interactive) (shell "*shell*<4>"))
+(defun shell-5 () (interactive) (shell "*shell*<5>"))
+
+(defun shell-6 () (interactive) (shell "*shell*<6>"))
+(defun shell-7 () (interactive) (shell "*shell*<7>"))
+(defun shell-8 () (interactive) (shell "*shell*<8>"))
+(defun shell-9 () (interactive) (shell "*shell*<9>"))
+; (global-set-key (kbd "C-c 1") (shell))
+
+; (global-set-key (kbd "C-c 2") (shell-2))
+
+(global-set-key "\C-c1" 'shell)
+(global-set-key "\C-c2" 'shell-2)
+(global-set-key "\C-c3" 'shell-3)
+(global-set-key "\C-c4" 'shell-4)
+
+
+(global-set-key "\C-c5" 'shell-5)
+(global-set-key "\C-c6" 'shell-6)
+(global-set-key "\C-c7" 'shell-7)
+(global-set-key "\C-c8" 'shell-8)
+(global-set-key "\C-c9" 'shell-9)
+
+(global-set-key "\M-m" 'compile)
+
+
+     (setq load-path
+           (append (list nil 
+			 "~/emacs-wiki-2.72"
+                         )
+                   load-path))
+
+
+(require 'emacs-wiki)
+
+(emacs-wiki-find-file "DailyLogJulyTwelve")
+
+
+
+
+;(add-to-list 'load-path "~/Downloads/ensime_2.9.0-0.6.0.RC1/elisp/" "~/Downloads/pinard-Pymacs-016b0bc")
+;(load-library "ensime")
+;(autoload 'pymacs-apply "pymacs")
+;(autoload 'pymacs-call "pymacs")
+;(autoload 'pymacs-eval "pymacs" nil t)
+;(autoload 'pymacs-exec "pymacs" nil t)
+;(autoload 'pymacs-load "pymacs" nil t)
+;;(eval-after-load "pymacs"
+;;  '(add-to-list 'pymacs-load-p
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Library Paths
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'load-path "~/.emacs.d" "~/python-mode.el-6.0.3")
+
+(setq ipython-command "/usr/local/bin/ipython")
+
+
+(require 'comint)
+(define-key comint-mode-map [(meta p)]
+   'comint-previous-matching-input-from-input)
+(define-key comint-mode-map [(meta n)]
+   'comint-next-matching-input-from-input)
+(define-key comint-mode-map [(control meta n)]
+    'comint-next-input)
+(define-key comint-mode-map [(control meta p)]
+    'comint-previous-input)
+
+(setq comint-completion-autolist t	;list possibilities on partial
+					;completion
+       comint-completion-recexact nil	;use shortest compl. if
+					;characters cannot be added
+       ;; how many history items are stored in comint-buffers (e.g. py-shell)
+       ;; use the HISTSIZE environment variable that shells use (if  avail.)
+       ;; (default is 32)
+       comint-input-ring-size (string-to-number (or (getenv  
+"HISTSIZE") "100")))
+
+
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(require 'ipython)
+(setq py-python-command-args '("-pylab" ))
+
+(require 'python-mode)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+
+;(require 'lambda-mode)
+;(add-hook 'python-mode-hook #'lambda-mode 1)
+;(setq lambda-symbol (string (make-char 'greek-iso8859-7 107)))
+
+;(require 'anything)
+;(require 'anything-ipython)
+
+;(when (require 'anything-show-completion nil t)
+;   (use-anything-show-completion 'anything-ipython-complete
+;                                 '(length initial-pattern)))
+;(require 'python-pep8)
+;(require 'python-pylint)
+;(require ‘anything-ipython)
+;(define-key py-mode-map (kbd "M-") 'anything-ipython-complete)
+;(define-key py-shell-map (kbd "M-") 'anything-ipython-complete)
+(define-key py-mode-map (kbd "C-c M") 'anything-ipython-import-modules-from-buffer)
+
+(dolist (key '(          
+               "\C-z"))
+  (global-unset-key key))
+
+  (display-time)
+
+  (when (load "flymake" t) 
+         (defun flymake-pyflakes-init () 
+           (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+                              'flymake-create-temp-inplace)) 
+              (local-file (file-relative-name 
+                           temp-file 
+                           (file-name-directory buffer-file-name)))) 
+             (list "/Users/tom/pychecker.sh" (list local-file)))) 
+
+         (add-to-list 'flymake-allowed-file-name-masks 
+                  '("\\.py\\'" flymake-pyflakes-init))) 
+
+   (add-hook 'find-file-hook 'flymake-find-file-hook)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(py-pychecker-command "/users/tom/pychecker.sh")
+ '(py-pychecker-command-args (quote ("")))
+ '(python-check-command "/users/tom/pychecker.sh")
+ '(send-mail-function (quote smtpmail-send-it))
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 587))
+
+(put 'upcase-region 'disabled nil)
+
+; (require 'websearch)
+(require 'jumper)
+
+(require 'gnus)
+
+ 	
+(setq gnus-select-method              '(nnimap "Gmail"
+                      (nnimap-address "imap.gmail.com")))
+
+(add-to-list 'gnus-secondary-select-methods
+             '(nnimap "Gmail"
+                      (nnimap-address "imap.gmail.com")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ emacs.d/jumper.el
@@ -0,0 +1,191 @@
+;;; jumper.el -- commands for jumping to source files.
+;;;
+;;; Copyright (c) 2012, Peter Seibel
+;;; All rights reserved.
+;;;
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions
+;;; are met:
+;;;
+;;;     * Redistributions of source code must retain the above
+;;;       copyright notice, this list of conditions and the following
+;;;       disclaimer.
+;;;
+;;;     * Redistributions in binary form must reproduce the above
+;;;       copyright notice, this list of conditions and the following
+;;;       disclaimer in the documentation and/or other materials
+;;;       provided with the distribution.
+;;;
+;;;     * Neither the name of Peter Seibel nor the names of its
+;;;       contributors may be used to endorse or promote products
+;;;       derived from this software without specific prior written
+;;;       permission.
+;;;
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+;;; CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+;;; INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+;;; MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
+;;; BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+;;; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+;;; TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+;;; DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+;;; ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+;;; TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+;;; THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+;;; SUCH DAMAGE.
+;;;
+
+;;; TODO: do something smart when there are multiple definitions for a
+;;; name.
+
+
+;; To get the minor mode for whatever kinds of files you've generated
+;; JUMPER files for do something like:
+;;
+;;   (add-hook 'ruby-mode-hook 'jumper-mode)
+;;
+;; in your .emacs.
+
+(require 'cl)
+(require 'etags)
+
+(defvar *jumper-default-jumper-file* "JUMPER")
+
+(defvar *jumper-patterns* ())
+
+(setq *jumper-patterns*
+  '(
+    "[[:blank:]]+\\([^ :]+\\):\\([[:digit:]]+\\):"     ; Scala compile errors
+    "[[:blank:]]*\\([^:]+\\):\\([[:digit:]]+\\)"      ; Ruby errors and grep -n output
+    "from \\([^:]+\\):\\([[:digit:]]+\\)"             ; Ruby stack trace
+    "at \\([^ ]+\\) line \\([[:digit:]]+\\)[,.]"      ; Perl errors
+    "in \\([^ ]+\\) on line \\([[:digit:]]+\\)"       ; PHP errors
+    "File \"\\([^\"]+\\)\", line \\([[:digit:]]+\\)," ; Python stack trace
+    "\\(.+\\)"
+    "[[:blank:]]*\\([^ ]+\\)"
+    "[[:blank:]]*\\([^:]+\\):"
+))
+
+(defun jumper-jump-to-symbol ()
+  "Jump to the definition of the symbol at the point."
+  (interactive)
+  (jumper-jump-to-def (symbol-name (symbol-at-point))))
+
+(defun jumper-jump-to-file ()
+  "Jump to the file we find named somewhere on the line by
+matching our *jumper-patterns* against the whole line."
+  (interactive)
+  (let ((line (jumper-line-as-string))
+        (found nil))
+    (dolist (pattern *jumper-patterns*)
+      (when (string-match pattern line)
+        (let ((name (expand-file-name (match-string 1 line)))
+              (line-num (match-string 2 line)))
+          (when (file-exists-p name)
+            (setq found t)
+            (message "Found %s %s with %s" name line-num pattern)
+            (when (jumper-jump-to name (if line-num (string-to-number line-num) nil))
+              (local-set-key (kbd "M-,") 'jumper-pop-definition-stack))))))
+    (unless found
+      (message "No file on line."))))
+
+(defun jumper-jump-to (file &optional line pattern)
+  "Jump to a particular file and, optionally, a particular line."
+  (cond
+   ((file-exists-p file)
+    (jumper-push-definition-stack)
+    (find-file file)
+    (when line
+      (goto-line line)
+      (when pattern
+        (re-search-forward pattern)
+        (goto-char (match-beginning 0))))
+    t)
+   (t
+    (message "No file: %s" file)
+    nil)))
+
+(defun jumper-find-def-in-file (file name)
+  (save-current-buffer
+    (set-buffer (find-file-noselect file))
+    (goto-char 0)
+    (let ((case-fold-search nil))
+      (search-forward-regexp (concat "^" name "\t")))
+    (destructuring-bind (name source-file line)
+        (split-string (jumper-line-as-string) "\t")
+      (list
+       (expand-file-name source-file (file-name-directory file))
+       (string-to-number line)))))
+
+(defun jumper-line-as-string ()
+  (save-excursion
+    (beginning-of-line)
+    (let ((start (point)))
+      (end-of-line)
+      (buffer-substring-no-properties start (point)))))
+
+(defun jumper-jump-to-def (name)
+  (destructuring-bind (file line) (jumper-find-def-in-file (jumper-find-jumper-file) name)
+    (jumper-jump-to file line name)))
+
+(defun jumper-up-dir (d)
+  (file-name-directory (directory-file-name d)))
+
+(defun jumper-find-jumper-file ()
+  (jumper-%find-jumper-file (buffer-file-name)))
+
+(defun jumper-%find-jumper-file (d)
+  (let ((filename (expand-file-name *jumper-default-jumper-file* d)))
+    (cond
+     ((jumper-is-file-p filename) filename)
+     ((string-equal d "/") nil)
+     (t (jumper-%find-jumper-file (jumper-up-dir d))))))
+
+(defun jumper-is-file-p (f)
+  (and (file-exists-p f)
+       (not (file-exists-p (file-name-as-directory f)))))
+
+;;; Next two functions based on code from from SLIME
+
+(defun jumper-push-definition-stack ()
+  "Add point to find-tag-marker-ring."
+  (cond
+   ((featurep 'xemacs) (push-tag-mark))
+   (t (ring-insert find-tag-marker-ring (point-marker)))))
+
+(defun jumper-pop-definition-stack ()
+  "Pop the edit-definition stack and goto the location."
+  (interactive)
+  (cond
+   ((featurep 'xemacs) (pop-tag-mark nil))
+   (t (pop-tag-mark))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Define a minor mode that can be used whenever we have JUMPER files
+;;; built.
+
+(defun jumper-mode-on () (jumper-mode t))
+(defun jumper-mode-on () (jumper-mode t))
+
+(define-minor-mode jumper-mode
+  "Jump to definitions of the current symbol"
+  nil
+  :lighter " jmp"
+  :global nil
+  :keymap
+  (list
+   (cons (kbd "M-.") 'jumper-jump-to-symbol)
+   (cons (kbd "M-,") 'jumper-pop-definition-stack)))
+
+(define-minor-mode jumper-line-mode
+  "Jump to file locations based on the current line."
+  nil
+  :lighter " jmp"
+  :global nil
+  :keymap
+  (list
+   (cons (kbd "M-.") 'jumper-jump-to-file)
+   (cons (kbd "M-,") 'jumper-pop-definition-stack)))
+
+(provide 'jumper)
